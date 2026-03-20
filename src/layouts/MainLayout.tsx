7b0f58d1dad04e26be082906974aa4940d783d3e
xzedm <zhuke04@mail.ru>
2026-03-20 08:35:01 +0500

    fixed errors

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
+        <header className="flex h-4 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
           </div>
         </header>
